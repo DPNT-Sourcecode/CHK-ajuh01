42e266f83e215ff8281c6c600d8e0c523c053503
sbx_user1051 <sbx_user1051@169.254.82.101>
2024-04-01 12:49:40 +0000

    Mon Apr 01 12:49:40 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,7 +3,47 @@ from collections import defaultdict
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+"""
+| A    | 50    | 3A for 130, 5A for 200 |
+| B    | 30    | 2B for 45              |
+| C    | 20    |                        |
+| D    | 15    |                        |
+| E    | 40    | 2E get one B free      |
+| F    | 10    | 2F get one F free      |
 
+| G    | 20    |                        |
+| H    | 10    | 5H for 45, 10H for 80  |
+| I    | 35    |                        |
+| J    | 60    |                        |
+| K    | 80    | 2K for 150             |
+| L    | 90    |                        |
+| M    | 15    |                        |
+| N    | 40    | 3N get one M free      |
+| O    | 10    |                        |
+| P    | 50    | 5P for 200             |
+| Q    | 30    | 3Q for 80              |
+| R    | 50    | 3R get one Q free      |
+| S    | 30    |                        |
+| T    | 20    |                        |
+| U    | 40    | 3U get one U free      |
+| V    | 50    | 2V for 90, 3V for 130  |
+| W    | 20    |                        |
+| X    | 90    |                        |
+| Y    | 10    |                        |
+| Z    | 50    |                        |
+
+"""
+
+item_pricing = {
+    "A":{"price": 50, "bundles": [(5, 200), (3, 130),],},
+"B":{"price": 30, "bundles": [(2, 45),],},
+"C":{"price": 20, "bundles": [],},
+"D":{"price": 15, "bundles": [],},
+"E":{"price": 40, "bundles": [], "free_item": [(2, "B")]},
+"F":{"price": 10, "bundles": [(3, 20)],},
+"G":{"price": 20, "bundles": [],},
+
+}
 
 def checkout(skus):
     basket = defaultdict(int)
@@ -38,3 +78,4 @@ def checkout(skus):
 
 
     return price
+
