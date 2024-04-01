a444d7b6ef9802ff8e7977cd7dc38e347398c2e2
sbx_user1051 <sbx_user1051@169.254.82.101>
2024-04-01 12:15:27 +0000

    Mon Apr 01 12:15:27 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,24 @@
 
-
+from collections import defaultdict
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+
 def checkout(skus):
-    raise NotImplementedError()
+    basket = defaultdict(int)
+    if not isinstance(skus, str):
+        return -1
+    for item in skus:
+        if item not in "ABCD":
+            return -1
+        basket[item] += 1
+
+    price = 0
+    for key, value in basket:
+        if key == "C":
+            price += value * 20
+        elif key == "D":
+            price += value * 15
+        elif key == "D" and value:
+            price += value * 15
+
