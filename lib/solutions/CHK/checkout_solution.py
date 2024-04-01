2111713acd793333196ee031fc98e47b9d777449
sbx_user1051 <sbx_user1051@169.254.82.101>
2024-04-01 12:28:12 +0000

    Mon Apr 01 12:28:12 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,22 +9,28 @@ def checkout(skus):
     if not isinstance(skus, str):
         return -1
     for item in skus:
-        if item not in "ABCD":
+        if item not in "ABCDE":
             return -1
         basket[item] += 1
 
     price = 0
+    e_values = basket["E"]
+
     for key, value in basket.items():
+
         if key == "C":
             price += value * 20
+        elif key == "E":
+            price += value * 40
         elif key == "D":
             price += value * 15
         elif key == "A":
             offer, value = divmod(value, 3)
             price += offer * 130 + value * 50
         elif key == "B":
+            value = max(0, value-e_values//2)
             offer, value = divmod(value, 2)
             price += offer * 45 + value * 30
+
+
     return price
-
-
